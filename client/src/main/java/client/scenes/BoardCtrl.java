a474a674f9b95162e9e00e5aae2ddd4d6d95640b
Tudor Măgirescu <magirescu@student.tudelft.nl>
2023-03-05 20:05:00 +0100

    Made the first card draggable

--- client/src/main/java/client/scenes/BoardCtrl.java
+++ client/src/main/java/client/scenes/BoardCtrl.java
@@ -1,14 +1,40 @@
 package client.scenes;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.input.*;
+import javafx.scene.layout.AnchorPane;
+
+import javax.inject.Inject;
 
 public class BoardCtrl {
+
+    @FXML
+    private AnchorPane card1Container;
+
+    @FXML
+    private Hyperlink card1;
+
+    private double originalX;
+    private double originalY;
+
+    public void dragDetected(MouseEvent mouseEvent) {
+        card1Container.getParent().toFront();
+        card1Container.toFront();
+    }
+
+    public void mousePressed(MouseEvent mouseEvent) {
+        originalX = mouseEvent.getX();
+        originalY = mouseEvent.getY();
+    }
+
+    public void mouseDragged(MouseEvent mouseEvent) {
+        card1Container.setLayoutX(card1Container.getLayoutX() + mouseEvent.getX() - originalX);
+        card1Container.setLayoutY(card1Container.getLayoutY() + mouseEvent.getY() - originalY);
+    }
+
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
 
 }
