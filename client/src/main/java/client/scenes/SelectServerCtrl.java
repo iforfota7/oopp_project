f30f368b5f5a59e857084213d83142cb7c571647
Emilia Ketterer <e.l.ketterer@student.tudelft.nl>
2023-03-23 18:16:57 +0100

    Changed imports to ensure the pipeline passes

--- client/src/main/java/client/scenes/SelectServerCtrl.java
+++ client/src/main/java/client/scenes/SelectServerCtrl.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import org.w3c.dom.Text;
 
 public class SelectServerCtrl {
 
