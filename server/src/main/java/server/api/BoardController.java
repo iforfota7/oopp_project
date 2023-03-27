12e58c0f229cd8e2ab0e2ce6ad861537d72b2adf
Ioana Forfota <I.Forfota@student.tudelft.nl>
2023-03-27 18:26:04 +0200

    Developing state of the feature of adding a new board

---
+++ server/src/main/java/server/api/BoardController.java
@@ -0,0 +1,37 @@
+package server.api;
+
+import commons.Boards;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import server.database.BoardsRepository;
+
+import javax.transaction.Transactional;
+
+@RestController
+@RequestMapping("api/boards")
+public class BoardController {
+    private final BoardsRepository repo;
+
+    public BoardController(BoardsRepository repo) {
+        this.repo = repo;
+    }
+
+    @Transactional
+    @PostMapping(path={"", "/"})
+    public ResponseEntity<Boards> addBoard(@RequestBody Boards board){
+        System.out.println("LAAAAAAALALALLAALLALA");
+        if(board == null || isNullOrEmpty(board.getName()))
+            return ResponseEntity.badRequest().build();
+
+        if(repo.existsById(Long.valueOf(board.name)))
+            return ResponseEntity.badRequest().build();
+
+        Boards saved = repo.save(board);
+
+        return ResponseEntity.ok(saved);
+    }
+
+    private static boolean isNullOrEmpty(String s) {
+        return s == null || s.isEmpty();
+    }
+}
