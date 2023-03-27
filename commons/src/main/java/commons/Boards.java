4b07b71f2f19ea7b11acff002210a3451cabcfe8
Lazar Polovina <L.Polovina@student.tudelft.nl>
2023-03-27 14:55:49 +0200

    JSON board's problem resolved

--- commons/src/main/java/commons/Boards.java
+++ commons/src/main/java/commons/Boards.java
@@ -1,10 +1,16 @@
 package commons;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import javax.persistence.*;
 import java.util.List;
 import java.util.Objects;
 
 @Entity
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "name")
 public class Boards {
     @Id
     public String name;
