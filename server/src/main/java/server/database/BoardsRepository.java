5cddaf1cda6ef6d8a49b2a8b4e4eceaca90c8020
Lazar Polovina <L.Polovina@student.tudelft.nl>
2023-03-29 22:10:16 +0200

    Threads resolved

--- server/src/main/java/server/database/BoardsRepository.java
+++ server/src/main/java/server/database/BoardsRepository.java
@@ -9,6 +9,10 @@ import java.util.Optional;
 public interface BoardsRepository extends JpaRepository<Boards, Long> {
 
 
+    /** FindByName searches for board with specific name...
+     * @param name - String name
+     * @return - Board with that name, null otherwise
+     */
     @Query(value = "SELECT * FROM BOARDS WHERE BOARDS.NAME = ?1",
             nativeQuery = true)
     Optional<Boards> findByName(String name);
