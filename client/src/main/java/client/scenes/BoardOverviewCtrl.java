8374a83343b89fb5f09f269a5f40900cad375952
Emilia Ketterer <e.l.ketterer@student.tudelft.nl>
2023-04-03 20:10:20 +0200

    Made the boardOverview scrollable and stopped the boards from resizing

--- client/src/main/java/client/scenes/BoardOverviewCtrl.java
+++ client/src/main/java/client/scenes/BoardOverviewCtrl.java
@@ -108,11 +108,8 @@ public class BoardOverviewCtrl{
         int positionInColumn = (numberOfBoards - 1) % 3;
         int row = (numberOfBoards - 1) / 3;
 
-
         StackPane newBoard = createNewBoard(b);
-
         newBoard.setAccessibleRole(AccessibleRole.TEXT);
-
         gridPane.add(newBoard, positionInColumn, row);
         gridPane.setMargin(gridPane.getChildren().get(numberOfBoards - 1),
                 new Insets(10, 10 , 10 ,10));
@@ -129,8 +126,10 @@ public class BoardOverviewCtrl{
         newBoard.setStyle("-fx-background-color: #ffffff; -fx-text-fill:  #0d0d0d; " +
                 "-fx-border-color: #8d78a6; -fx-border-radius: 3px; -fx-text-fill: #000000;" +
                 "-fx-z-index: 999;");
-        newBoard.setPrefWidth(165);
-        newBoard.setPrefHeight(75);
+        newBoard.setPrefWidth(160.8);
+        newBoard.setPrefHeight(73.6);
+        newBoard.setMinWidth(160.8);
+        newBoard.setMinHeight(73.6);
         newBoard.setAlignment(Pos.CENTER);
         newBoard.setText(b.name);
         newBoard.getProperties().put("board", b);
