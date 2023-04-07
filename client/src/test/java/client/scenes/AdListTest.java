b52e2eab97413dbdf6888f6e8139132e14f1fddb
Tudor Măgirescu <magirescu@student.tudelft.nl>
2023-04-07 14:52:53 +0200

    Renamed the test classes because naming was ambiguous

---
+++ client/src/test/java/client/scenes/AdListTest.java
@@ -0,0 +1,43 @@
+package client.scenes;
+
+import javafx.scene.layout.HBox;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdListCtrlTest {
+
+    private MockBoardCtrl mockBoardCtrl;
+    private MockMainCtrl mockMainCtrl;
+    private MockAdListCtrl mockAdListCtrl;
+    private AdListCtrlServices sut;
+
+    @BeforeEach
+    public void setup() {
+        mockBoardCtrl = new MockBoardCtrl();
+        mockMainCtrl = new MockMainCtrl();
+        sut = new AdListCtrlServices();
+        mockAdListCtrl = new MockAdListCtrl(mockBoardCtrl, mockMainCtrl, sut);
+        sut.setCtrl(mockAdListCtrl, mockBoardCtrl, mockMainCtrl);
+    }
+
+    @Test
+    public void emptyTitle() {
+        assertFalse(sut.saveNewList(""));
+        assertTrue(mockAdListCtrl.warning.isVisible());
+        assertFalse(mockAdListCtrl.calledMethods.contains("setNewListName"));
+        assertFalse(mockMainCtrl.calledMethods.contains("closeSecondaryStage"));
+    }
+
+    @Test
+    public void nonEmptyTitle() {
+        mockBoardCtrl.firstRow.getChildren().add(new HBox());
+
+        assertTrue(sut.saveNewList("title"));
+        assertFalse(mockAdListCtrl.warning.isVisible());
+        assertTrue(mockAdListCtrl.calledMethods.contains("setNewListName"));
+        assertTrue(mockMainCtrl.calledMethods.contains("closeSecondaryStage"));
+        assertTrue(mockBoardCtrl.calledMethods.contains("addListToBoard 1"));
+    }
+}
