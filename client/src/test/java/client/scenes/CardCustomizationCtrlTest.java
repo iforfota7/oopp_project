df8d2bc209301b583d75740f3d43f1912125cb31
Ziliang Zhang <z.zhang-78@student.tudelft.nl>
2023-04-11 16:25:58 +0200

    fixed review

--- client/src/test/java/client/scenes/CardCustomizationCtrlTest.java
+++ client/src/test/java/client/scenes/CardCustomizationCtrlTest.java
@@ -12,24 +12,24 @@ class CardCustomizationCtrlTest {
     private MockServerUtils testServerUtils;
     private MockMainCtrl testMainCtrl;
     private MockBoardCtrl testBoardCtrl;
-    private CardDetailsCtrl sut;
+    private CardDetailsCtrl cardDetailsCtrl;
 
     private BoardCtrl boardCtrl;
 
-    private CardCustomizationCtrl ccc;
+    private CardCustomizationCtrl sut;
 
     @BeforeEach
     public void setUp() {
         testBoardCtrl = new MockBoardCtrl();
         testServerUtils = new MockServerUtils();
         testMainCtrl = new MockMainCtrl();
-        sut =new CardDetailsCtrl(testServerUtils, testMainCtrl);
-        ccc= new MockCardCustomizationCtrl(testBoardCtrl,testMainCtrl,testServerUtils,sut);
+        cardDetailsCtrl =new CardDetailsCtrl(testServerUtils, testMainCtrl);
+        sut = new CardCustomizationCtrl(testBoardCtrl,testMainCtrl,testServerUtils, cardDetailsCtrl);
     }
 
     @Test
     void closeTest() {
-        ccc.close();
+        sut.close();
         assertTrue(testMainCtrl.calledMethods.contains("closeStage"));
     }
 
